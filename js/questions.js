b8094468c9b3e90352981499c62f3a4319dcd71c
Prasad Ashok Chavan <askokc4321@gmail.com>
2022-02-07 22:19:40 +0530

    Quiz Site HTML/ CSS/ JS
    .....><......

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+//array for questions , options and numbers & answers
+ let questions = [
+    {
+        numb: 1,
+        question: "When is world science day celebrated ?",
+        answer: "November 10",
+        options: [
+            "November 18",
+            "November 10",
+            "January 07",
+            "March 07",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which year is the first world science day celebrated ?",
+        answer: "November 10, 2002",
+        options: [
+            "November 10, 2002",
+            "November 10, 2001",
+            "November 10, 2003",
+            "November 18, 2003",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who established the world science day ?",
+        answer: "UNESCO",
+        options: [
+            "UNESCO",
+            "World Health Organization",
+            "CISCO",
+            "NASA LAB",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Why is world science day celebrated ?",
+        answer: "Promote national and international solidarity.",
+        options: [
+            "To draw attention to the challenges faced by science.",
+            "Promote national and international solidarity.",
+            "Improve Science & Technology.",
+            "Strengthen public awareness on role of Science.",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the theme of the world science day for 2019 ?",
+        answer: "Open Science, leaving no one behind.",
+        options: [
+            "Due to struglling with Covi-19.",
+            "Human Right.",
+            "Towards green societies.",
+            "Open Science, leaving no one behind.",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Science day is celebrated in India to Honour:",
+        answer: "Sir CV Raman",
+        options: [
+            "Sir CV Raman",
+            "C.P. Srinivasa Ramanujan",
+            "Homi J. Bhabha",
+            "Jagadish Chandra bose",
+        ]
+    },
+    {
+        numb: 7,
+        question: "Sir C.V. Raman awarded Nobel Prize on ... was a First Asian person.",
+        answer: "1930",
+        options: [
+            "1956",
+            "1933",
+            "1930",
+            "1935",
+        ]
+    },
+    {
+        numb: 8,
+        question: "UNESCO Headquarter is located in ?",
+        answer: "Paris, France.",
+        options: [
+            "Geneva, Swizerland.",
+            "Washington, USA.",
+            "London, England.",
+            "Paris, France.",
+        ]
+    },
+    {
+        numb: 9,
+        question: "Who invented Zero ?",
+        answer: "Aarya Bhatta.",
+        options: [
+            "Aarya Bhatta.",
+            "BrahamaGupta.",
+            "Maharshi Walmik.",
+            "Issac Newton.",
+        ]
+    },
+    {
+        numb: 10,
+        question: "Who invented Black Hole Theory ?",
+        answer: "Stephen Hawking.",
+        options: [
+            "Issac Newton.",
+            "Stephen Hawking.",
+            "Bohr & Frontland Lab.",
+            "None of Above.",
+        ]
+    },
+ ];
